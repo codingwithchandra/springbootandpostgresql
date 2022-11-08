bd063c96fdbae4343ca5c8c62c60ebf5b049fdfa
erchandrasekhar <er.chandrasekharyadav@gmail.com>
2022-11-08 18:31:00 +0530

    first commit

---
+++ src/main/java/com/coding_with_chandra/SpringbootandpostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.coding_with_chandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootandpostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootandpostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding_with_chandra/SpringbootandpostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding_with_chandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootandpostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
